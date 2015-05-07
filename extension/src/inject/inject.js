615c81b730c6672e89160ee67a095729522db4c0
Sebastiaan van Arkens <sebastiaan@vanarkens.nl>
2015-05-07 03:21:51 +0200

    Added functionality for blacklisting and disabling tags

--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -3,11 +3,32 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
+		//Check if Glamorama box is there
 		if($( "#glamorama" ).length > 0)
 		{
+			//Remove Glamorama box
 			$("#glamorama").remove();
+			//Message that it has been removed
 			console.log('Glamorama gone!');
 		}
+
+		//Array with users, needs to come from settings
+		var users = [''];
+
+		$.each(users, function(key, value)
+		{
+			//Find all comments on post for user with username
+			$('.comment footer:contains("'+value+'")').each(function()
+			{
+				//Remove the comment
+				$(this).parent().hide();
+			});
+		});
+
+		$('.dump-tags ul').children().hide();
+		$('.dump-tags ul').html('<li>Tags staan momenteel uit, open de Glam-0-Drama extentie om de instellingen aan te passen</li>');
+
+
 	}
 	}, 10);
 });
